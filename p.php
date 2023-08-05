e012dcc9437eee5459600685f97a4db5bbc9af8e
tnt <132759560+tnt6213274o6923@users.noreply.github.com>
2023-08-05 22:34:04 +0700

    m

---
+++ p.php
@@ -0,0 +1,17 @@
+<?php
+$m = $_POST['m'];
+
+$conn = new mysqli('localhost','root','','test');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
